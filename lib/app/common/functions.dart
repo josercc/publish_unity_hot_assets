fc92e837e647168c812b4b279d1b5e67d19d24eb
josercc <josercc@163.com>
2026-04-30 13:42:45 +0800

    重构配置(pubspec)：将版本更新至2.3.6
    - 将项目版本更新为2.3.6。
    - 重构错误和成功提示弹窗消息，改用自定义组件以优化展示样式。
    - 更新Podfile.lock文件，同步FlutterMacOS校验值与Podfile校验值的变更。

--- lib/app/common/functions.dart
+++ lib/app/common/functions.dart
@@ -1,11 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:quickalert/quickalert.dart';
+
+Widget _buildAlertMessage(String msg) {
+  return SizedBox(
+    width: double.infinity,
+    child: ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 260),
+      child: SingleChildScrollView(
+        child: Text(
+          msg,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+  );
+}
 
 void showErrorToast(String msg) {
   QuickAlert.show(
     context: Get.context!,
     type: QuickAlertType.error,
-    title: msg,
+    // title: msg,
+    widget: _buildAlertMessage(msg),
   );
 }
 
@@ -13,6 +30,7 @@ void showSuccessToast(String msg) {
   QuickAlert.show(
     context: Get.context!,
     type: QuickAlertType.success,
-    title: msg,
+    // title: msg,
+    widget: _buildAlertMessage(msg),
   );
 }
